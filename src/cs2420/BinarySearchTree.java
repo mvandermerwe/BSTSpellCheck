372031195119f22af82ec781cc10caa5f0f2283d
Mark Van der Merwe <vandermerwem2@gmail.com>
2017-02-25 18:55:49 -0700

    "progress" on BinarySearchTree

--- src/cs2420/BinarySearchTree.java
+++ src/cs2420/BinarySearchTree.java
@@ -4,7 +4,11 @@
 package cs2420;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
@@ -15,27 +19,31 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 
 	protected Node<Type> root;
 	private int size = 0;
-	
+
 	/**
-	 * FIXME: comments
+	 * Protected for unit testing.
 	 *
 	 * Pictorially, a node is:
 	 *
-	 *       left data  right
-	 *       --------------- 
-	 *     <--+   |  5  |  +--->
-	 *       --------------- 
-	 *  
-	 *  Note, while a 5 is used above any "Type" could be contained in the node
+	 * left data right --------------- <--+ | 5 | +---> ---------------
+	 * 
+	 * Note, while a 5 is used above any "Type" could be contained in the node
 	 */
-	static class Node<Type> {
-		
+	protected static class Node<Type extends Comparable<? super Type>> {
+
 		Type data;
 		Node<Type> left;
 		Node<Type> right;
 
-		// FIXME: write a constructor that simplifies building an initial node
-		Node(Type the_data);
+		/**
+		 * constructor that simplifies building an initial node
+		 * 
+		 * @param the_data
+		 *            - data
+		 */
+		Node(Type the_data) {
+			data = the_data;
+		}
 
 		/**
 		 * 
@@ -48,7 +56,21 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 		 * 
 		 * 
 		 */
-		int height();
+		int height() {
+			int leftHeight = 0;
+			int rightHeight = 0;
+			// Base case both are null - return 0 + 1;
+			if (left != null) {
+				leftHeight = left.height();
+			}
+			if (right != null) {
+				rightHeight = right.height();
+			}
+
+			// Find bigger of two heights.
+			int biggerNumber = (leftHeight < rightHeight) ? rightHeight : leftHeight;
+			return biggerNumber + 1;
+		}
 
 		/**
 		 * recursive determine if the item is in this node or the nodes after
@@ -57,7 +79,21 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 		 *            - needle
 		 * @return true if item in tree
 		 */
-		boolean contains(Type item);
+		boolean contains(Type item) {
+			if (item.compareTo(data) == 0) {
+				return true;
+			} else if (item.compareTo(data) < 0) {
+				// Go left.
+				if (left == null)
+					return false;
+				return left.contains(item);
+			} else {
+				// Go right.
+				if (left == null)
+					return false;
+				return right.contains(item);
+			}
+		}
 
 		/**
 		 * recursive - add a node
@@ -66,89 +102,178 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 		 *            - data to add
 		 * @return
 		 */
-		void insert(Type item);
+		void insert(Type item) {
+			// Node is part of set, no duplicates allowed.
+			if (item.compareTo(data) == 0)
+				return;
+			else if (item.compareTo(data) < 0) {
+				// Go left.
+				if (left == null) {
+					left = new Node<Type>(item);
+				}
+				left.insert(item);
+			} else {
+				// Go right.
+				if (right == null) {
+					right = new Node<Type>(item);
+				}
+				right.insert(item);
+			}
+
+		}
 
 	}
-	
+
 	public BinarySearchTree(Type item) {
 		root = new Node<>(item);
 		size = 1;
 	}
-	
+
 	public BinarySearchTree() {
 		// The defaults of the properties are already in proper
 		// state for an empty tree
 	}
-	
+
 	@Override
 	public boolean add(Type item) {
-		// TODO Auto-generated method stub
-		return false;
+		// Technically don't have to - for clarity.
+		// item will be dereferenced in contains().
+		if (item == null) {
+			throw new NullPointerException();
+		}
+
+		if (root.contains(item)) {
+			return false;
+		} else {
+			root.insert(item);
+			size++;
+			return true;
+		}
 	}
 
 	@Override
 	public boolean addAll(Collection<? extends Type> items) {
-		// TODO Auto-generated method stub
-		return false;
+		// Technically don't have to - for clarity.
+		if (items == null) {
+			throw new NullPointerException();
+		}
+
+		// Collections.shuffle(items); -> We gon shuffle her
+		boolean isChanged = false;
+		for (Type item : items) {
+			if (add(item))
+				isChanged = true;
+		}
+		return isChanged;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		root = null;
+		size = 0;
+		// Let garbage be collected.
 	}
 
 	@Override
 	public boolean contains(Type item) {
-		// TODO Auto-generated method stub
-		return false;
+		// Technically don't have to do this but clarity is nice.
+		if (item == null) {
+			throw new NullPointerException();
+		}
+		return root.contains(item);
 	}
 
 	@Override
 	public boolean containsAll(Collection<? extends Type> items) {
-		// TODO Auto-generated method stub
-		return false;
+		for(Type item: items) {
+			if(!contains(item)) return false;
+		}
+		return true;
 	}
 
 	@Override
 	public Type first() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if(root == null) {
+			throw new NoSuchElementException();
+		}
+		
+		// follow the left path all the way down...
+		Node<Type> currentNode = root;
+		while(currentNode.left != null) {
+			currentNode = currentNode.left;
+		}
+		
+		return currentNode.data;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return root == null;
 	}
 
 	@Override
 	public Type last() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if(root == null) {
+			throw new NoSuchElementException();
+		}
+		
+		// follow the right path all the way down...
+		Node<Type> currentNode = root;
+		while(currentNode.right != null) {
+			currentNode = currentNode.right;
+		}
+		
+		return currentNode.data;
 	}
 
 	@Override
 	public boolean remove(Type item) {
-		// TODO Auto-generated method stub
-		return false;
+		if(item == null) {
+			throw new NullPointerException();
+		}
+		if (root == null) return false;
+		
+		Node<Type> currentNode = null;
+		Node<Type> nextNode = root;
+		while(nextNode != null) {
+			int comparisonData = item.compareTo(nextNode.data);
+			if (comparisonData == 0) {
+				//currentNode
+			} else if (comparisonData < 0) {
+				//check if null - false
+				// go left
+				nextNode = nextNode.left;
+			} else {
+				//check if null - false
+				// go right
+				nextNode = nextNode.right;
+			}
+			currentNode = nextNode;
+		}
+		
+		
 	}
 
 	@Override
 	public boolean removeAll(Collection<? extends Type> items) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean isChanged = false;
+		for(Type item: items) {
+			if(remove(item)) isChanged = true;
+		}
+		return isChanged;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 	@Override
 	public ArrayList<Type> toArrayList() {
-		// TODO Auto-generated method stub
+		BinarySearchTree temp = this;
+		ArrayList<Type> treeArrayList = new ArrayList<>();
+		//treeArrayList.add(temp.first());
+		temp.remove(first());
 		return null;
 	}
 
--- src/cs2420/BinarySearchTreeTest.java
+++ src/cs2420/BinarySearchTreeTest.java
@@ -20,13 +20,13 @@ public class BinarySearchTreeTest {
 
 	BinarySearchTree.Node<Integer> sampleNode;
 	BinarySearchTree.Node<Integer> oneNode;
-	
+
 	BinarySearchTree<Integer> sampleTree;
 	BinarySearchTree<Integer> oneNodeTree;
 	BinarySearchTree<Integer> emptyTree;
-	
+
 	List<Integer> listOfNumbers;
-	
+
 	/**
 	 * Create some nodes and trees to work with.
 	 */
@@ -36,77 +36,83 @@ public class BinarySearchTreeTest {
 		sampleNode.insert(5);
 		sampleNode.insert(9);
 		sampleNode.insert(11);
-		
+
 		oneNode = new BinarySearchTree.Node<>(18);
-		
+
 		sampleTree = new BinarySearchTree<>(2);
 		sampleTree.add(5);
 		sampleTree.add(-1);
 		sampleTree.add(7);
 		sampleTree.add(4);
 		sampleTree.add(3);
-		
+
 		oneNodeTree = new BinarySearchTree<>(4);
 		emptyTree = new BinarySearchTree<>();
-		
-		Integer[] numbersToAdd = {10,20,30};
+
+		Integer[] numbersToAdd = { 10, 20, 30 };
 		listOfNumbers = Arrays.asList(numbersToAdd);
 
 	}
-	
+
 	// --------- INNER NODE CLASS TESTS ----------
-	
+
+	@Test
 	public void testNodeConstructor() {
 		BinarySearchTree.Node<Integer> node = new BinarySearchTree.Node<>(5);
 		assertTrue(5 == node.data);
 	}
-	
+
+	@Test
 	public void testHeight() {
-		assertEquals(3,sampleNode.height());
-		assertEquals(1,oneNode.height());
+		assertEquals(3, sampleNode.height());
+		assertEquals(1, oneNode.height());
 	}
-	
+
+	@Test
 	public void testContains() {
 		assertTrue(sampleNode.contains(9));
 		assertTrue(sampleNode.contains(5));
-		
+
 		assertFalse(sampleNode.contains(10));
-		
+
 		assertTrue(oneNode.contains(18));
 	}
-	
+
+	@Test
 	public void testInsert() {
 		oneNode.insert(2);
 		assertTrue(2 == oneNode.left.data);
-		
+
 		sampleNode.insert(10);
-		assertEquals(4,sampleNode.height());
-		
+		assertEquals(4, sampleNode.height());
+
 	}
-	
+
 	// -------- Binary Search Tree Tests --------
-	
+
+	@Test
 	public void testBSTConstructor() {
 		BinarySearchTree<Integer> newTree = new BinarySearchTree<>(1);
 		assertTrue(1 == newTree.root.data);
-		assertEquals(1,newTree.size());
+		assertEquals(1, newTree.size());
 	}
-	
+
+	@Test
 	public void testAdd() {
 		assertTrue(sampleTree.add(8));
 		assertTrue(sampleTree.contains(8));
-		assertEquals(7,sampleTree.size());
-		
+		assertEquals(7, sampleTree.size());
+
 		assertTrue(oneNodeTree.add(130));
 		assertTrue(sampleTree.contains(130));
-		assertEquals(2,sampleTree.size());
-		
+		assertEquals(2, sampleTree.size());
+
 		assertTrue(emptyTree.add(1));
 		assertTrue(sampleTree.contains(1));
-		assertEquals(1,sampleTree.size());
-		
+		assertEquals(1, sampleTree.size());
+
 		assertFalse(oneNodeTree.add(4));
-		
+
 		// test for null
 		try {
 			oneNodeTree.add(null);
@@ -115,18 +121,19 @@ public class BinarySearchTreeTest {
 			// test passes!!
 		}
 	}
-	
+
+	@Test
 	public void testAddAll() {
-		
+
 		assertTrue(sampleTree.addAll(listOfNumbers));
 		assertFalse(sampleTree.addAll(listOfNumbers));
-		
+
 		assertTrue(oneNodeTree.addAll(listOfNumbers));
 		assertFalse(oneNodeTree.addAll(listOfNumbers));
-		
+
 		assertTrue(emptyTree.addAll(listOfNumbers));
 		assertFalse(emptyTree.addAll(listOfNumbers));
-		
+
 		// test for null
 		listOfNumbers.add(1, null);
 		try {
@@ -136,30 +143,32 @@ public class BinarySearchTreeTest {
 			// test passes!!
 		}
 	}
-	
+
+	@Test
 	public void testClear() {
 		sampleTree.clear();
-		assertEquals(0,sampleTree.size());
+		assertEquals(0, sampleTree.size());
 		assertTrue(null == sampleTree.root);
-		
+
 		oneNodeTree.clear();
-		assertEquals(0,oneNodeTree.size());
+		assertEquals(0, oneNodeTree.size());
 		assertTrue(null == oneNodeTree.root);
-		
+
 		emptyTree.clear();
-		assertEquals(0,emptyTree.size());
+		assertEquals(0, emptyTree.size());
 		assertTrue(null == emptyTree.root);
 	}
-	
+
+	@Test
 	public void contains() {
 		assertTrue(sampleTree.contains(4));
 		assertFalse(sampleTree.contains(1024));
-		
+
 		assertTrue(oneNodeTree.contains(4));
 		assertFalse(oneNodeTree.contains(-253));
-		
+
 		assertFalse(emptyTree.contains(9));
-		
+
 		try {
 			sampleTree.contains(null);
 			fail("Failed to throw exception on null contains");
@@ -168,20 +177,21 @@ public class BinarySearchTreeTest {
 		}
 
 	}
-	
+
+	@Test
 	public void testContainsAll() {
 		assertFalse(sampleTree.containsAll(listOfNumbers));
 		sampleTree.addAll(listOfNumbers);
 		assertTrue(sampleTree.containsAll(listOfNumbers));
-		
+
 		assertFalse(oneNodeTree.containsAll(listOfNumbers));
 		oneNodeTree.addAll(listOfNumbers);
 		assertTrue(oneNodeTree.containsAll(listOfNumbers));
-		
+
 		assertFalse(emptyTree.containsAll(listOfNumbers));
 		emptyTree.addAll(listOfNumbers);
 		assertTrue(emptyTree.containsAll(listOfNumbers));
-	
+
 		listOfNumbers.add(1, null);
 		try {
 			oneNodeTree.containsAll(listOfNumbers);
@@ -190,11 +200,12 @@ public class BinarySearchTreeTest {
 			// test passes!!
 		}
 	}
-	
+
+	@Test
 	public void testFirst() {
 		assertTrue(-1 == sampleTree.first());
 		assertTrue(4 == oneNodeTree.first());
-		
+
 		try {
 			emptyTree.first();
 			fail("Failed to throw exeption on getting from empty tree");
@@ -202,11 +213,12 @@ public class BinarySearchTreeTest {
 			// test passes!!
 		}
 	}
-	
+
+	@Test
 	public void testLast() {
 		assertTrue(7 == sampleTree.last());
 		assertTrue(4 == sampleTree.last());
-		
+
 		try {
 			emptyTree.last();
 			fail("Failed to throw exeption on getting from empty tree");
@@ -215,23 +227,25 @@ public class BinarySearchTreeTest {
 		}
 	}
 
+	@Test
 	public void testIsEmpty() {
 		assertFalse(sampleTree.isEmpty());
 		assertFalse(oneNodeTree.isEmpty());
 		assertTrue(emptyTree.isEmpty());
 	}
-	
+
+	@Test
 	public void remove() {
 		assertTrue(sampleTree.remove(4));
 		assertFalse(sampleTree.contains(4));
 		assertFalse(sampleTree.remove(2048));
-		
+
 		assertTrue(oneNodeTree.remove(4));
 		assertTrue(oneNodeTree.isEmpty());
 		assertFalse(oneNodeTree.remove(4096));
-		
+
 		assertFalse(emptyTree.remove(8192));
-		
+
 		try {
 			sampleTree.remove(null);
 			fail("Failed to throw exception when removing null");
@@ -239,12 +253,13 @@ public class BinarySearchTreeTest {
 			// test passes!!
 		}
 	}
-	
+
+	@Test
 	public void removeAll() {
 		assertFalse(sampleTree.removeAll(listOfNumbers));
 		sampleTree.addAll(listOfNumbers);
 		assertTrue(sampleTree.removeAll(listOfNumbers));
-		
+
 		assertFalse(oneNodeTree.removeAll(listOfNumbers));
 		oneNodeTree.addAll(listOfNumbers);
 		assertTrue(oneNodeTree.removeAll(listOfNumbers));
@@ -252,7 +267,7 @@ public class BinarySearchTreeTest {
 		assertFalse(emptyTree.removeAll(listOfNumbers));
 		emptyTree.addAll(listOfNumbers);
 		assertTrue(emptyTree.removeAll(listOfNumbers));
-		
+
 		listOfNumbers.add(1, null);
 		try {
 			oneNodeTree.removeAll(listOfNumbers);
@@ -261,23 +276,25 @@ public class BinarySearchTreeTest {
 			// test passes!!
 		}
 	}
-	
+
+	@Test
 	public void testSize() {
-		assertEquals(6,sampleTree.size());
-		assertEquals(1,oneNodeTree.size());
-		assertEquals(0,emptyTree.size());
+		assertEquals(6, sampleTree.size());
+		assertEquals(1, oneNodeTree.size());
+		assertEquals(0, emptyTree.size());
 	}
-	
+
+	@Test
 	public void testToArrayList() {
 		List<Integer> sampleList = sampleTree.toArrayList();
-		assertEquals(6,sampleList.size());
-		assertEquals("[-1,2,3,4,5,7]",sampleList.toString());
-		
+		assertEquals(6, sampleList.size());
+		assertEquals("[-1,2,3,4,5,7]", sampleList.toString());
+
 		List<Integer> oneNodeList = oneNodeTree.toArrayList();
-		assertEquals(1,oneNodeList.size());
+		assertEquals(1, oneNodeList.size());
 		assertTrue(oneNodeList.contains(4));
-		
+
 		List<Integer> emptyList = emptyTree.toArrayList();
-		assertEquals(0,emptyList.size());
+		assertEquals(0, emptyList.size());
 	}
 }

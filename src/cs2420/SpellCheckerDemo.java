de5c15569fc36019a034a022e9ee07514b78fd4c
JJ Garzella <jjgarzella@gmail.com>
2017-03-01 13:35:23 -0700

    Modified Spell checker to work, it also doesn't need tests

--- src/cs2420/SpellCheckerDemo.java
+++ src/cs2420/SpellCheckerDemo.java
@@ -10,25 +10,25 @@ import java.util.List;
  */
 public class SpellCheckerDemo {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    SpellChecker mySC = new SpellChecker(new File("dictionary.txt"));
+		SpellChecker mySC = new SpellChecker(new File("Resources/dictionary.txt"));
 
-    run_spell_check(mySC, "hello_world.txt");
-    run_spell_check(mySC, "good_luck.txt");
-  }
+		run_spell_check(mySC, "Resources/hello_world.txt");
+		run_spell_check(mySC, "Resources/good_luck.txt");
+	}
 
-  private static void run_spell_check(SpellChecker sc, String documentFilename) {
+	private static void run_spell_check(SpellChecker sc, String documentFilename) {
 
-    File doc = new File(documentFilename);
-    List<String> misspelledWords = sc.spellCheck(doc);
-    if (misspelledWords.size() == 0) {
-      System.out.println("There are no misspelled words in file " + doc + ".");
-    } else {
-      System.out.println("The misspelled words in file " + doc + " are:");
-      for (String w : misspelledWords) {
-        System.out.println("\t" + w);
-      }
-    }
-  }
+		File doc = new File(documentFilename);
+		List<String> misspelledWords = sc.spellCheck(doc);
+		if (misspelledWords.size() == 0) {
+			System.out.println("There are no misspelled words in file " + doc + ".");
+		} else {
+			System.out.println("The misspelled words in file " + doc + " are:");
+			for (String w : misspelledWords) {
+				System.out.println("\t" + w);
+			}
+		}
+	}
 }
